839b51695e25b2f095123dc6c5c9d750ff86601f
Michael <mike.michaelmilstead@gmail.com>
2024-06-15 07:51:33 -0700

    add line graph props

--- demo/frontend-hydra-demo/src/app/components/line-graph.tsx
+++ demo/frontend-hydra-demo/src/app/components/line-graph.tsx
@@ -1,44 +1,58 @@
 import { useMemo } from "react";
 import { AxisOptions, Chart } from "react-charts";
 
-type DailyStars = {
-  date: Date;
-  stars: number;
-};
-
-type Series = {
-  label: string;
-  data: DailyStars[];
-};
-
-function generateDataSeries(
-  label: string,
-  startStars: number,
-  startDate: Date,
-  days: number
-): Series {
-  const data: DailyStars[] = [];
-  let currentStars = startStars;
-
-  for (let i = 0; i < days; i++) {
-    currentStars += Math.floor(Math.random() * 100); // Increment stars by a random amount between 0 and 100
-    data.push({
-      date: new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000), // Increment date by one day
-      stars: currentStars,
-    });
-  }
-
-  console.log(data);
-
-  return { label, data };
+interface LineGraphProps {
+  backgroundColor: string;
+  titleClassName: string;
+  title: string;
+  descriptionClassName: string;
+  description: string;
 }
 
-const data: Series[] = [
-  generateDataSeries("SeriesA", 20, new Date(2024, 0, 1), 10),
-  generateDataSeries("SeriesB", 102, new Date(2024, 0, 1), 10),
-];
+export default function LineGraph({
+  backgroundColor,
+  titleClassName,
+  title,
+  descriptionClassName,
+  description,
+}: LineGraphProps) {
+  type DailyStars = {
+    date: Date;
+    stars: number;
+  };
 
-export default function LineGraph() {
+  type Series = {
+    label: string;
+    data: DailyStars[];
+  };
+
+  function generateDataSeries(
+    label: string,
+    startStars: number,
+    startDate: Date,
+    days: number
+  ): Series {
+    const data: DailyStars[] = [];
+    let currentStars = startStars;
+
+    for (let i = 0; i < days; i++) {
+      currentStars += Math.floor(Math.random() * 100); // Increment stars by a random amount between 0 and 100
+      data.push({
+        date: new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000), // Increment date by one day
+        stars: currentStars,
+      });
+    }
+
+    console.log(data);
+
+    return { label, data };
+  }
+
+  const data: Series[] = [
+    generateDataSeries("SeriesA", 20, new Date(2024, 0, 1), 10),
+    generateDataSeries("SeriesB", 102, new Date(2024, 0, 1), 10),
+  ];
+
   const primaryAxis = useMemo(
     (): AxisOptions<DailyStars> => ({
       getValue: (datum) => datum.date,
@@ -56,7 +70,9 @@ export default function LineGraph() {
   );
 
   return (
-    <div className="h-[300px] w-full bg-white rounded-md">
+    <div className={`h-[300px] w-full ${backgroundColor} rounded-md`}>
+      <h1 className={titleClassName}>{title}</h1>
+      <p className={descriptionClassName}>{description}</p>
       <Chart
         options={{
           data,
