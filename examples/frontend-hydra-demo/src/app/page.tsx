866f326130b8bcce81f0c111016256283c863571
Michael <mike.michaelmilstead@gmail.com>
2024-06-15 05:09:49 -0700

    init demo frontend

---
+++ examples/frontend-hydra-demo/src/app/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          maxWidth: 1100,
+        }}
+      >
+        <h1>Template</h1>
+      </div>
+    </main>
+  );
+}
