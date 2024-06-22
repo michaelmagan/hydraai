4dcb1ac4d16bdec7b869719261a77dd98a46597e
Michael <mike.michaelmilstead@gmail.com>
2024-06-22 00:40:40 -0700

    only init hydra once

--- demo/frontend-hydra-demo/src/app/services/component-gen.service.ts
+++ demo/frontend-hydra-demo/src/app/services/component-gen.service.ts
@@ -1,12 +1,17 @@
 "use server";
 
+import Hydra from "hydra-ai-backup";
 import { ReactElement } from "react";
 import { initHydra } from "./hydra";
+
+let hydra: Hydra | null;
 
 export const generateDynamicMessage = async (
   message: string
 ): Promise<ReactElement> => {
-  const hydra = initHydra(process.env.OPENAI_API_KEY ?? "");
+  if (!hydra) {
+    hydra = initHydra(process.env.OPENAI_API_KEY ?? "");
+  }
   const response = await hydra.generateComponent(message);
   console.log("Response:", response);
   return response;
