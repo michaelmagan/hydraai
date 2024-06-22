12a8ab930de0b9b8620e53b921cd5d890ba1d560
Michael <mike.michaelmilstead@gmail.com>
2024-06-22 06:38:23 -0700

    style quote component

--- demo/frontend-hydra-demo/src/app/components/historical-quote.tsx
+++ demo/frontend-hydra-demo/src/app/components/historical-quote.tsx
@@ -5,19 +5,10 @@ const HistoricalQuote: React.FC<{
   author: string;
   year: string;
 }> = ({ quote, author, year }) => (
-  <div
-    style={{
-      border: "1px solid #FFC107",
-      padding: "15px",
-      borderRadius: "10px",
-      margin: "15px 0",
-      backgroundColor: "#FFF9E6",
-      color: "#FFC107",
-    }}
-  >
-    <blockquote style={{ margin: "0 0 10px 0" }}>
-      <p>{quote}</p>
-      <footer>
+  <div className="p-4 rounded-lg my-4 bg-white text-gray-700">
+    <blockquote className="mb-2">
+      <p className="mb-2">{quote}</p>
+      <footer className="text-sm">
         — {author}, {year}
       </footer>
     </blockquote>
