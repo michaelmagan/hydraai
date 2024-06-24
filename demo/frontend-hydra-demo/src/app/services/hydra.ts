aeed900de37504030d427a4625d8746f01f9dd9f
michaelmagan <michael.magan92@gmail.com>
2024-06-24 13:29:54 -0700

    wip: using published packaged isntead of linked

--- demo/frontend-hydra-demo/src/app/services/hydra.ts
+++ demo/frontend-hydra-demo/src/app/services/hydra.ts
@@ -1,4 +1,4 @@
-import Hydra from "hydra-ai"; // no more lint errors, but nextjs is not recognizing it as a package...
+import Hydra from "hydra-ai";
 
 import ProfilePhotosRow from "../components/profile-photos-row";
 import { ProfilePhotoPropsArray } from "../components/types";
@@ -7,7 +7,6 @@ export const initHydra = (openAIKey: string) => {
   const hydra = new Hydra(openAIKey);
 
   hydra.registerComponent("profilephotosrow", ProfilePhotosRow, {
-    // getting a lint error saying this isn't the right type.
     photos: ProfilePhotoPropsArray,
   });
   return hydra;
