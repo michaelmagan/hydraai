3b6525445a736a3ec7f95b8ab27c324432ba80a6
michaelmagan <michael.magan92@gmail.com>
2024-06-24 13:41:12 -0700

    wip: fixed the server only error + added the get openai key.

--- demo/frontend-hydra-demo/src/app/services/component-gen.service.ts
+++ demo/frontend-hydra-demo/src/app/services/component-gen.service.ts
@@ -1,18 +1,18 @@
+"use server";
+
 import { DynamicMessage } from "../model/dynamic-message";
 import { initHydra } from "./hydra";
 
 export const generateDynamicMessage = async (
   message: string
 ): Promise<DynamicMessage> => {
-  const hydra = initHydra("your-openai-key"); // Replace with your actual OpenAI key
+  const hydra = initHydra(process.env.OPENAI_API_KEY ?? "");
   const response = await hydra.generateComponent(message);
-  console.log("Response:", response);
-
   console.log("Response:", response);
   return {
     who: "bot",
     message: message,
-    type: response.availableComponents.name,
-    componentData: response.availableComponents.props,
+    type: "test",
+    componentData: response,
   };
 };
