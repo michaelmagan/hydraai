4e1eb2791a21ebe64e12790f83b1870f1799145a
Michael <mike.michaelmilstead@gmail.com>
2024-07-16 01:31:14 -0700

    add discussions

--- examples/mage-demo/src/app/hydra-client.ts
+++ examples/mage-demo/src/app/hydra-client.ts
@@ -1,6 +1,8 @@
 import { HydraClient } from "hydra-ai";
+import { DiscussionList } from "./components/discussion-list";
 import { ProfileCardList } from "./components/profile-card-list";
 import { SendMessageList } from "./components/send-message-list";
+import { getDiscussions } from "./services/discussion-service";
 import { getProfiles } from "./services/profile-service";
 
 const hydra = new HydraClient();
@@ -17,5 +19,15 @@ hydra.registerComponent(
 hydra.registerComponent("SendMessageList", SendMessageList, {
   messages: "{id: string, to: string, message: string}[]",
 });
+
+hydra.registerComponent(
+  "DiscussionList",
+  DiscussionList,
+  {
+    discussions:
+      "{id: string, title: string, description: string, createdDateIso: string, messages: {id: string, discussionId: string, from: string, message: string, createdDateIso: string}[]}[]",
+  },
+  getDiscussions
+);
 
 export default hydra;
