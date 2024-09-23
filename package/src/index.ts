a947851d0026275d7315d72a74cb776e8ed46528
Michael <mike.michaelmilstead@gmail.com>
2024-09-22 21:44:17 -0700

    export HydraBackend

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./hydra-ai/helper-components/hydra-chat";
+export { default as HydraBackend } from "./hydra-ai/hydra-ai-backend";
 export { default as HydraClient } from "./hydra-ai/hydra-ai-client";
 export { chooseComponent, saveComponent } from "./hydra-ai/hydra-server-action";
 export * from "./hydra-ai/model";
